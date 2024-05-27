5f8a14c519a6f96070e7015d8c728ea68ef0fe8a
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-05-28 01:43:26 +0300

    passage through isort

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
-from src.masks import mask_card, mask_check
 from typing import Union
+
+from src.masks import mask_card, mask_check
 
 
 def mask_type_card_check(coming_str: Union[str]) -> Union[str]:
