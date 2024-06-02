0443d7df3d330160f4ac48817be0b8c3615fb02f
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-06-02 18:09:30 +0300

    add function sort_date_id_list

--- src/processing.py
+++ src/processing.py
@@ -11,3 +11,10 @@ def new_list_pass_value(id_list: Iterable, default_state: str = "EXECUTED") -> I
             new_id_list.append(dict_list)
     return new_id_list
 
+
+def sort_date_id_list(id_list: Iterable, reverse_list: bool = True) -> Iterable:
+    """Функция, которая принимает список словарей и возвращает список,
+    в котором исходные словари отсортированы по убыванию даты (ключ date),
+    функция принимает два аргумента, второй задает порядок сортировки (убывание)"""
+    sort_date_list = sorted(id_list, key=lambda date_dick: date_dick["date"], reverse=reverse_list)
+    return sort_date_list
