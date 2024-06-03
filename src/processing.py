1e2742137431678991c6e303e8150daf5ea3aa66
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-06-03 14:36:57 +0300

    add corrected processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,14 @@
-from typing import Iterable
+from typing import Any, Iterable
 
 
-def new_list_pass_value(id_list: Iterable, default_state: str = "EXECUTED") -> Iterable:
+def new_list_pass_value(id_list: list[dict[str, Any]], default_state: bool = True) -> list[dict[str, Any]]:
     """Функция, которая принимает список словарей и значение для ключа
     'state' и возвращает новый список, содержащий только те словари, у которых ключ
     'state' содержит переданное в функцию значение"""
     new_id_list = []
-    for dict_list in id_list:
-        if dict_list["state"] == default_state:
-            new_id_list.append(dict_list)
+    for dict_list_id in id_list:
+        if dict_list_id.get("state") == default_state:
+            new_id_list.append(dict_list_id)
     return new_id_list
 
 
@@ -16,5 +16,5 @@ def sort_date_id_list(id_list: Iterable, reverse_list: bool = True) -> Iterable:
     """Функция, которая принимает список словарей и возвращает список,
     в котором исходные словари отсортированы по убыванию даты (ключ date),
     функция принимает два аргумента, второй задает порядок сортировки (убывание)"""
-    sort_date_list = sorted(id_list, key=lambda date_dick: date_dick["date"], reverse=reverse_list)
+    sort_date_list = sorted(id_list, key=lambda date_dick: date_dick("date"), reverse=reverse_list)
     return sort_date_list
