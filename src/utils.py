d9a929a6cac9cff59c234355839d221dadd8691c
Vasiliy Sochnev <vasiliysochnev@gmail.com>
2024-06-19 14:57:40 +0300

    add func get_transactions_data in utils.py

---
+++ src/utils.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+
+def get_transactions_data(file_path: str = []) -> list:
+    """Функция, которая принимает на вход путь до JSON-файла
+    и возвращает список словарей с данными о финансовых транзакциях."""
+
+    if not os.path.exists(file_path):
+        return []
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            transactions_json_data = json.load(f)
+
+        if isinstance(transactions_json_data, list):
+            return transactions_json_data
+
+        else:
+            return []
+
+    except (json.JSONDecodeError, OSError):
+        return []
+
+
+if __name__ == "__main__":
+    file_path = "..\\data\\operations.json"
+    transactions = get_transactions_data(file_path)
+    print(transactions)
